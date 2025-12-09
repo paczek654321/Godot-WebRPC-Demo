18c53018ca83b6d26e1d4932e0525b203b00d478
paczek654321 <paczek654321@gmail.com>
2025-12-09 16:59:18 +0100

    Added a GUI display for messages; Made instances send messages to each other on connect

--- main_menu.gd
+++ main_menu.gd
@@ -1,10 +1,20 @@
 extends "res://webrtc_client.gd"
 
 var input: String:
-	get(): return $VBoxContainer/Input.text
+	get():
+		if $VBoxContainer/Input.text == "":
+			return $VBoxContainer/Input.placeholder_text
+		return $VBoxContainer/Input.text
 
 func join() -> void:
 	$VBoxContainer/HBoxContainer/Join.disabled = true
 	start()
 
 func message() -> void: send_message(input)
+
+func _ready() -> void:
+	super()
+	message_recieved.connect(display_message)
+
+func display_message(text: String) -> void:
+	$output.text += text + "\n"
--- webrtc_client.gd
+++ webrtc_client.gd
@@ -3,6 +3,8 @@ extends Node
 var channel: WebRTCDataChannel
 var connection := WebRTCPeerConnection.new()
 
+signal message_recieved
+
 func _ready() -> void: set_process(false)
 
 func start() -> void:
@@ -20,12 +22,13 @@ func start() -> void:
 	var signaling_manager := SignalingManager.new(get_tree(), connection, "ws://localhost:8080")
 	
 	await signaling_manager.connected
+	send_message("Connected")
 
 func _process(_delta: float) -> void:
 	connection.poll()
 	if channel.get_ready_state() == WebRTCDataChannel.STATE_OPEN:
 		while channel.get_available_packet_count() > 0:
-			print("received: ", channel.get_packet().get_string_from_utf8())
+			message_recieved.emit(channel.get_packet().get_string_from_utf8())
 
 func send_message(message: String) -> void:
 	channel.put_packet(message.to_utf8_buffer())
